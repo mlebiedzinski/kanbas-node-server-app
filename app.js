a2e2d63a068001ec57e241d2ffb749e3833b5916
mlebiedzinski <lebiedzinski.m@northeastern.edu>
2023-12-04 20:47:24 -0500

    a6

--- app.js
+++ app.js
@@ -2,18 +2,39 @@ import express from 'express'
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js"
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb+srv://mlebiedzinski:<password>@cluster0.bwcvhye.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(CONNECTION_STRING);
 import "dotenv/config";
 import session from "express-session";
-const app = express()
-app.use(cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL
-})
+const app = express();
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(
+    session(sessionOptions)
 );
 CourseRoutes(app);
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
 Hello(app)
